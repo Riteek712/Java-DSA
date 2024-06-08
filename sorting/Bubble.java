e3e337b6de1fc81b9333fee843067b1bb1b740b1
Riteek Rakesh <riteek7.12@gmail.com>
2024-06-08 10:48:33 +0530

    Sorting: BUbble sort

---
+++ sorting/Bubble.java
@@ -0,0 +1,27 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class Bubble {
+    public static int[] bubbleSort( int[] arr){
+        int n =  arr.length;
+        for(int i = 0; i< n-1; i++){ // We need N - 1 iterations to sort the array.
+            for(int j = 0; j < n-1-i; j++ ){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5,7,5,4,10,-2, 6,3};
+        arr = bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+        
+    }
+}
