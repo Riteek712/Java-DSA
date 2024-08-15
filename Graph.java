f444482b5878fb7c02c03b6fff5af8baf84e4b8c
Riteek Rakesh <riteek7.12@gmail.com>
2024-08-15 08:25:20 +0530

    bfs for graph

--- Graph.java
+++ Graph.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Graph {
     // Graph is network of nodes.
@@ -14,6 +16,8 @@ public class Graph {
      * - Adjency list or matrix, edge list , 2d matrix (implicit graph)
      * 
      * Adjacency List: list of lists
+     * 
+     * BFS can also be called indirect level order travesal.
      */
 
 
@@ -21,9 +25,11 @@ public class Graph {
      static class Edge{
         int src;
         int dest;
-        public Edge(int s, int d){
+        int weight;
+        public Edge(int s, int d, int w){
             this.src = s;
             this.dest = d;
+            this.weight = w;
         }
      }
 
@@ -32,28 +38,60 @@ public class Graph {
             graph[i] = new ArrayList<Edge>();
         }
 
-        graph[0].add(new Edge(0,2));
+        graph[0].add(new Edge(0,1, 2));
+        graph[0].add(new Edge(0,2, 2));
 
-        graph[1].add(new Edge(1,2));
-        graph[1].add(new Edge(1,3));
+        graph[1].add(new Edge(1,0,10));
+        graph[1].add(new Edge(1,3,0));
 
-        graph[2].add(new Edge(2,0));
-        graph[2].add(new Edge(2,1));
-        graph[2].add(new Edge(2,3));
+        graph[2].add(new Edge(2,0, 2));
+        graph[2].add(new Edge(2,4, 0));
+        // graph[2].add(new Edge(2,3, -1));
 
-        graph[3].add(new Edge(3,1));
-        graph[3].add(new Edge(3,2));
+        graph[3].add(new Edge(3,1, 0));
+        graph[3].add(new Edge(3,4, 0));
+        graph[3].add(new Edge(3,5, 0));
+
+        graph[3].add(new Edge(4,2, 0));
+        graph[3].add(new Edge(4,5, 0));
+        graph[3].add(new Edge(4,3, 0));
+
+        graph[3].add(new Edge(5,3, 0));
+        graph[3].add(new Edge(5,4, 0));
+        graph[3].add(new Edge(5,6, 0));
      }
+     // BFS :breadth first search
+     public static void bfs(ArrayList<Edge> graph[], int V){
+        Queue<Integer> q = new LinkedList<>();
+        boolean vis[] = new boolean[V];
+        q.add(0);
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            if(!vis[curr]){
+                System.out.print(curr+" ");
+                vis[curr] = true;
+
+                for(int i =0; i<graph[curr].size(); i++){
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
+     }
+     
      public static void main(String args[]){
-        int V =4;
+        int V =7;
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
 
         //print 2's neighbors
         for(int i = 0; i< graph[1].size(); i++){
             Edge e = graph[1].get(i);
-            System.out.println(e.dest+" ");
+            System.out.println(e.dest+", "+e.weight);
         }
+
+        System.out.println("BFS Traversal");
+        bfs(graph, V);
      }
 
 }
