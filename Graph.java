d67aa981c1168f44f682e0668cd83207f66e6427
Riteek Rakesh <riteek7.12@gmail.com>
2024-08-15 08:45:16 +0530

    DFS for graph

--- Graph.java
+++ Graph.java
@@ -78,6 +78,17 @@ public class Graph {
             }
         }
      }
+
+     public static void dfs(ArrayList<Edge> graph[], int curr,boolean vis[]){
+        System.out.print(curr+" "); 
+        vis[curr] = true;
+
+        for(int i = 0; i <graph[curr].size();i++){
+            Edge e = graph[curr].get(i);
+            if(!vis[e.dest]){dfs(graph, e.dest, vis);}
+            
+        }
+     }
      
      public static void main(String args[]){
         int V =7;
@@ -92,6 +103,10 @@ public class Graph {
 
         System.out.println("BFS Traversal");
         bfs(graph, V);
+        System.out.println();
+        System.out.println("DFS Traversal");
+        boolean vis[] = new boolean[V];
+        dfs(graph, 0, vis);
      }
 
 }
