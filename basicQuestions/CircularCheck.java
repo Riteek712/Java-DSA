6c0990570516cb1b44df09b8e4da653dd6873525
Riteek Rakesh <riteek7.12@gmail.com>
2024-08-25 20:31:58 +0530

    added problem statement

--- basicQuestions/CircularCheck.java
+++ basicQuestions/CircularCheck.java
@@ -1,6 +1,12 @@
 package basicQuestions;
 
 import java.util.Scanner;
+/*
+ Given an array arr[] of length N, the task is to check whether the given array is circularly sorted or not, we need to check whether the given array is the rotated form of the sorted array. 
+
+    - In ascending circularly sorted array, there will be at most one case where the element just before the current element will be greater than the current element i.e., arr[ i – 1 ] > arr[ i ]. If there is no such case, then we consider the array as circularly sorted, this is the result of a rotation by N steps. On the other hand, if there is only one such case; we then make sure that the first element of the array is greater than the last one i.e., arr[0] > arr[N-1].
+    - So we need to count the total existence of arr[ i – 1 ] > arr[ i ] cases and if the count is 1 with arr[ 0 ] < arr[ N-1 ] or if the count is greater than 1 then the result will be false else the result will be true meaning that the array is circularly sorted.
+ */
 
 public class CircularCheck {
     public static boolean isCircularCheck(int[] arr){
