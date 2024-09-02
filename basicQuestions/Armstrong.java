cdf3c1f9f26c413558abe1ec3c04e48e287b9269
Riteek Rakesh <riteek7.12@gmail.com>
2024-09-02 19:01:29 +0530

    improved second largest and smallest code

--- basicQuestions/Armstrong.java
+++ basicQuestions/Armstrong.java
@@ -7,22 +7,6 @@ public class Armstrong {
         return x*x*x;
     }
     public static boolean check(int n){
-        // int originalno = n;
-        // int count = 0;
-        // int temp = n;
-        // while (temp != 0)
-        // {
-        //     count++;
-        //     temp = temp / 10;
-        // }
-        // int sumofpower = 0;
-        // while (n != 0)
-        // {
-        //     int digit = n % 10;
-        //     sumofpower += Math.pow(digit,count);
-        //     n /= 10;
-        // }
-        // return (sumofpower == originalno);
         int originalno = n;
         int count = 0;
         int temp = n;
